aebf5fc998e61b197aad6583b05f2e12c8be2b5f
lambda-world <lambda.the.world@gmail.com>
2022-07-11 20:01:15 +0800

    feat: add some examples

---
+++ examples/crypto/index.ts
@@ -0,0 +1,45 @@
+import {
+    env,
+    BlockNumber,
+    RandomResult,
+    FixedArray16,
+    FixedArray32,
+    blake2x256,
+    blake2x128,
+    keccak256,
+    sha2x256,
+    Hash
+} from "ask-lang";
+
+@contract
+export class Contract {
+    constructor() {}
+
+    @constructor()
+    default(): void {}
+
+    @message()
+    random(input: Array<u8>): RandomResult<Hash, BlockNumber> {
+        return env().random<Hash, BlockNumber>(input);
+    }
+
+    @message()
+    blake2x256(input: Array<u8>): FixedArray32<u8> {
+        return blake2x256<Array<u8>, FixedArray32<u8>>(input);
+    }
+
+    @message()
+    blake2x128(input: Array<u8>): FixedArray16<u8> {
+        return blake2x128<Array<u8>, FixedArray16<u8>>(input);
+    }
+
+    @message()
+    keccak256(input: Array<u8>): StaticArray<u8> {
+        return keccak256<Array<u8>, StaticArray<u8>>(input);
+    }
+
+    @message()
+    sha2x256(input: Array<u8>): FixedArray32<u8> {
+        return sha2x256<Array<u8>, FixedArray32<u8>>(input);
+    }
+}
---
+++ examples/lazy/index.ts
@@ -0,0 +1,22 @@
+import { Lazy } from "ask-lang";
+
+@contract()
+export class Contract {
+    // read storage when used
+    _value: Lazy<u32> = new Lazy();
+
+    constructor() {}
+
+    @constructor()
+    default(): void {}
+
+    @message({ mutates: true })
+    set(v: u32): void {
+        this._value.set(v);
+    }
+
+    @message({ mutates: false })
+    get(): u32 {
+        return this._value.get();
+    }
+}
