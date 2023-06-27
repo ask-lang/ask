1d8a11d091dff72f6609671003921cdb883e4d36
yjhmelody <yjh465402634@gmail.com>
2023-06-27 14:53:09 +0800

    improve ci

---
+++ scripts/build-examples.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+yarn example ./examples/crypto
+yarn example ./examples/erc20
+yarn example ./examples/flipper
+yarn example ./examples/inherent-flipper
+yarn example ./examples/lazy
+yarn example ./examples/mapping
+yarn example ./examples/vector
